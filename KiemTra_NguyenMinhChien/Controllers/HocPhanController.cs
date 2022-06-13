2cddc9f62e0497121b2fec7d7fd4bf3e5a2d65aa
chiennguyen22052001@gmail.com <chiennguyen22052001@gmail.com>
2022-06-13 16:15:12 +0700

    updata

---
+++ KiemTra_NguyenMinhChien/Controllers/HocPhanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_NguyenMinhChien;
+using KiemTra_NguyenMinhChien.Models;
+
+namespace KiemTra_NguyenMinhChien.Controllers
+{
+    public class HocPhanController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        // GET: HocPhan
+        public ActionResult Index()
+        {
+            var khoahoc = from sv in data.HocPhans select sv;
+            return View(khoahoc);
+        }
+    }
+}
